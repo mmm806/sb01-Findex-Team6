e9c727517ada42b765641b5fb42f8ac965017ad4
realitsyourman <realitsyourman@gmail.com>
2025-03-13 16:06:16 +0900

    init

---
+++ src/test/java/com/sprint/findex_team6/FindexTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.findex_team6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindexTeam6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
