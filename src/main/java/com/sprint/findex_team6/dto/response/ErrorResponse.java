5f85c477eb971947ce0757c35de6796c36bb3d37
LeeMinJu <mmnnii0276@gmail.com>
2025-03-15 08:42:10 +0900

    fix: error response field name

--- src/main/java/com/sprint/findex_team6/dto/response/ErrorResponse.java
+++ src/main/java/com/sprint/findex_team6/dto/response/ErrorResponse.java
@@ -3,7 +3,7 @@ package com.sprint.findex_team6.dto.response;
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
-    LocalDateTime timestampExpand,
+    LocalDateTime timestamp,
     Integer status,
     String message,
     String details
