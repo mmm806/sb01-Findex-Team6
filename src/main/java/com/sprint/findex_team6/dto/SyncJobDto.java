2ce5dc2f3ae4c880de3278121429f6c2b9e2a9c6
realitsyourman <realitsyourman@gmail.com>
2025-03-15 10:23:41 +0900

    refactor: add builder SyncJobDto

--- src/main/java/com/sprint/findex_team6/dto/SyncJobDto.java
+++ src/main/java/com/sprint/findex_team6/dto/SyncJobDto.java
@@ -4,7 +4,9 @@ package com.sprint.findex_team6.dto;
 import com.sprint.findex_team6.entity.ContentType;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record SyncJobDto(
     Long id,
     ContentType jobType,
