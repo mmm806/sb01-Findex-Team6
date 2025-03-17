fb04155237a85e1100cbf89bc9376bef46eef19d
Kim EungJin <mmm806@naver.com>
2025-03-17 17:58:36 +0900

    feat: 지수 정보 조회 기능 추가 (IndexRepository)

--- src/main/java/com/sprint/findex_team6/repository/IndexRepository.java
+++ src/main/java/com/sprint/findex_team6/repository/IndexRepository.java
@@ -1,9 +1,13 @@
 package com.sprint.findex_team6.repository;
 
+import com.sprint.findex_team6.dto.IndexInfoSummaryDto;
 import com.sprint.findex_team6.entity.Index;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +16,12 @@ public interface IndexRepository extends JpaRepository<Index, Long> {
   Optional<Index> findByIndexName(String indexName);
 
   List<Index> findAllByIdIn(Collection<Integer> ids);
+
+  Optional<Index> findById(Long id);
+
+  Page<Index> findAll(Pageable pageable);
+
+  List<Index> findByIdGreaterThan(Long idAfter, Pageable pageable);
+
+  List<IndexInfoSummaryDto> findAllProjectBy();
 }
