a84e71159e49e4226d97b9fb7e3c6f206a0a45ed
Kim EungJin <mmm806@naver.com>
2025-03-21 17:35:08 +0900

    fix: 메서드 추가

--- src/main/java/com/sprint/findex_team6/mapper/IndexMapper.java
+++ src/main/java/com/sprint/findex_team6/mapper/IndexMapper.java
@@ -1,6 +1,7 @@
 package com.sprint.findex_team6.mapper;
 
 import com.sprint.findex_team6.dto.IndexInfoDto;
+import com.sprint.findex_team6.dto.IndexInfoSummaryDto;
 import com.sprint.findex_team6.entity.Index;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,4 +10,6 @@ import org.mapstruct.Mapping;
 public interface IndexMapper {
   @Mapping(source = "baseDate", target = "basePointInTime")
   IndexInfoDto toDto(Index index);
+
+  IndexInfoSummaryDto toSummaryDto(Index index);
 }
